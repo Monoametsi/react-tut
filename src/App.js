d22a9e5f99a79786659075aebf4e2c084ca22aa8
Monoametsi Mosemeng <216026633@student.uj.ac.za>
2022-01-24 17:01:37 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="container-fluid shadow rounded p-3 mt-3 w-50">
+    <div>
+      <h1>Create account</h1>
     </div>
-  );
+    <div className="container-fluid">
+      <div>
+        <h2>Your name</h2>
+        <input type="text" placeholder="Enter your name" />
+      </div>
+      <div>
+        <h2>Email</h2>
+        <input type="email" placeholder="Enter your email" />
+      </div>
+      <div>
+        <h2>Password</h2>
+        <input type="password" placeholder="Enter your password" />
+      </div>
+      <div>
+        <h2>Confirm password</h2>
+        <input type="text" placeholder="Re-enter your password" />
+      </div>
+        <div className="btn-cont">
+          <button className="btn btn-primary">Create your movie spot account</button>
+        </div>
+    </div>
+  </div>
+  )
 }
 
 export default App;
