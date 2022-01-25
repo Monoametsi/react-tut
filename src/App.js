f3db7f453e9e2a3f99c4d6850b51ff5604311342
Monoametsi Mosemeng <216026633@student.uj.ac.za>
2022-01-25 16:21:07 +0200

    Added changes

--- src/App.js
+++ src/App.js
@@ -6,20 +6,26 @@ function App() {
   const [ userExists, userAdded ] = useState(false);
 
   const submitUser = () => {
-    console.log(userExists);
+   userAdded(userExist => {
+      if(userExist){
+        return  userExist = false;
+      }else{
+        return userExist = true;
+      }
+    });
   }
 
   return (
   <div>
     <div className="container-sm shadow rounded p-3 mt-3 w-50">
-      <div>
+      <div className="mb-4">
         <h2>Create account</h2>
       </div>
       <div className="container-fluid-sm">
           
           <Input_field label="Full Name" inputType="text" />
           <Input_field label="Email" inputType="email" />
-          <Input_field label="Password" inputType="password" />
+          <Input_field label="Password(7 characters minimum)" inputType="password" />
           <Input_field label="Confirm Password" inputType="password" />
 
           <div className="btn-cont w-100">
@@ -28,7 +34,7 @@ function App() {
       </div>
     </div>
     
-    <User name="MJ" email="myEmail@gmail.com"/>
+    { userExists ? <User name="MJ" email="myEmail@gmail.com" /> : null}
   </div>
   )
 }
--- src/components/user.js
+++ src/components/user.js
@@ -1,6 +1,6 @@
 function User(prop){
     return (
-        <div className="d-flex border flex-column container shadow rounded p-0 w-50 mt-3">
+        <div className="d-flex border flex-column container shadow rounded p-0 w-50 my-3">
             <div className="d-flex justify-content-center p-2">
                 <h2>User</h2>
             </div>
@@ -12,6 +12,11 @@ function User(prop){
                 <span className="fs-4 fw-light pe-2">Email:</span>
                 <span className="fs-4 font-weight-bold">{prop.email}</span>
             </div>
+
+            <div className="btn-cont w-100 p-2 d-flex justify-content-between">
+            <button className="btn btn-primary w-50 me-2">Update {prop.name} details</button>
+            <button className="btn btn-danger w-50 ms-2">Delete {prop.name}</button>
+          </div>
         </div>
     )
 }
