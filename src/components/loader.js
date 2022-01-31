c0914939e1b112618ed7aa2924d7b06d459c6fbe
Junior <216026633@student.uj.ac.za>
2022-01-31 02:56:33 +0200

    Create loader spinner for loading events

---
+++ src/components/loader.js
@@ -0,0 +1,9 @@
+function loader(){
+	return (
+		<div className="d-flex justify-content-center align-items-center container-fluid bg-dark bg-opacity-75 position-fixed w-100 h-100 start-0 end-0 top-0 bottom-0">
+			<div className="loader border spinner-border border-5 p-4 rounded-circle border-light w-80 h-80"></div>
+		</div>
+	)
+}
+
+export default loader;
