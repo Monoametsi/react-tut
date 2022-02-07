a007976524697813c20343fa7d4347c2cd7be114
Monoametsi Mosemeng <216026633@student.uj.ac.za>
2022-02-07 16:59:29 +0200

    Add edit container

--- frontend/src/pages/edit-profile.js
+++ frontend/src/pages/edit-profile.js
@@ -1,10 +1,38 @@
 import Dasboard_header from '../components/dashboard-header'
-import Dasboard_box_link from '../components/dashboard-box-link'
 
 function Edit_profile() {
   return (
     <div>
       <Dasboard_header title="Edit Settings" Class="d-none" />
+
+      <div className='d-flex justify-content-center mt-3'>
+        <ul className='list-group shadow w-25'>
+          <li className='list-group-item d-flex justify-content-between  align-items-center'>
+            <div className='d-flex flex-column'>
+              <span>Name:</span>
+              <span>Monoametsi</span>
+            </div>
+
+            <div>
+              <button className='btn btn-primary'>
+                  Edit
+              </button>
+            </div>
+          </li>
+          <li className='list-group-item d-flex justify-content-between  align-items-center'>
+            <div className='d-flex flex-column'>
+              <span>Password:</span>
+              <span>*******</span>
+            </div>
+
+            <div>
+              <button className='btn btn-primary'>
+                  Edit
+              </button>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
