87428e35f77220e4c4bd41e09224a6ed05e92c43
Monoametsi Junior <216026633@student.uj.ac.za>
2022-02-09 00:04:31 +0200

    Add form component for user detail updates

--- frontend/src/pages/edit-profile.js
+++ frontend/src/pages/edit-profile.js
@@ -1,35 +1,36 @@
 import Dasboard_header from '../components/dashboard-header'
+import { Link } from 'react-router-dom';
 
 function Edit_profile() {
   return (
     <div>
       <Dasboard_header title="Edit Settings" Class="d-none" />
 
-      <div className='d-flex justify-content-center mt-3'>
-        <ul className='list-group shadow w-25'>
+      <div className='d-flex justify-content-center align-items-center mt-4'>
+        <ul className='list-group shadow w-50'>
           <li className='list-group-item d-flex justify-content-between  align-items-center'>
             <div className='d-flex flex-column'>
-              <span>Name:</span>
+              <span className="fw-bold">Name:</span>
               <span>Monoametsi</span>
             </div>
 
-            <div>
+            <Link to="/edit-profile/name">
               <button className='btn btn-primary'>
                   Edit
               </button>
-            </div>
+            </Link>
           </li>
           <li className='list-group-item d-flex justify-content-between  align-items-center'>
             <div className='d-flex flex-column'>
-              <span>Password:</span>
+              <span className="fw-bold">Password:</span>
               <span>*******</span>
             </div>
 
-            <div>
+            <Link to="/edit-profile/password">
               <button className='btn btn-primary'>
                   Edit
               </button>
-            </div>
+            </Link>
           </li>
         </ul>
       </div>
